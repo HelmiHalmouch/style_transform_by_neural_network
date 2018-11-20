80fedc23022937448aef4ad882fbd18e428d1155
HelmiHalmouch <helmirealmadrid@gmail.com>
2018-11-20 21:44:00 +0100

    add function to run the backward metod and test the model

--- main_algo_style_trans.py
+++ main_algo_style_trans.py
@@ -175,6 +175,80 @@ def get_style_model_and_losses(cnn, normalization_mean, normalization_std,
 
     return model, style_losses, content_losses
 
+# You can use a different image, but it has to have the same dimension as the other images.
+
+input_img = content_img.clone()
+"""plt.figure()
+imshow(input_img, title='Input Image')"""
+
+# add an optimizer 
+""" L-BFGS algorithm to run our gradient descent."""
+""" We create a PyTorch L-BFGS optimizer optim.LBFGS and pass the image as the tensor to optimize. 
+We use .requires_grad_() to ensure that the image requires gradient."""
+
+def get_input_optimizer(input_img):
+    optimizer = optim.LBFGS([input_img.requires_grad_()])
+    return optimizer
+
+#run the backward methods of each loss to dynamically compute their gradients and perform gradient descent.
+def run_style_transfer(cnn, normalization_mean, normalization_std,
+                       content_img, style_img, input_img, num_steps=300,
+                       style_weight=1000000, content_weight=1):
+   
+    model, style_losses, content_losses = get_style_model_and_losses(cnn,
+        normalization_mean, normalization_std, style_img, content_img)
+    optimizer = get_input_optimizer(input_img)
+
+    print('Optimizing..')
+    run = [0]
+    while run[0] <= num_steps:
+
+        def closure():
+            # correct the values of updated input image
+            input_img.data.clamp_(0, 1)
+
+            optimizer.zero_grad()
+            model(input_img)
+            style_score = 0
+            content_score = 0
+
+            for sl in style_losses:
+                style_score += sl.loss
+            for cl in content_losses:
+                content_score += cl.loss
+
+            style_score *= style_weight
+            content_score *= content_weight
+
+            loss = style_score + content_score
+            loss.backward()
+
+            run[0] += 1
+            if run[0] % 50 == 0:
+                print("run {}:".format(run))
+                print('Style Loss : {:4f} Content Loss: {:4f}'.format(
+                    style_score.item(), content_score.item()))
+                print()
+
+            return style_score + content_score
+
+        optimizer.step(closure)
+
+  
+    input_img.data.clamp_(0, 1)
+
+    return input_img
+
+# run the model and show the result (modified output image )
+output = run_style_transfer(cnn, cnn_normalization_mean, cnn_normalization_std,
+                            content_img, style_img, input_img)
+
+plt.figure()
+imshow(output, title='Output Image')
+
+plt.ioff()
+plt.show()
+
 
 print('OK OK')
-# TODO / ADD THE Loading the neural network
+# TODO : optimisation of the code representation 
